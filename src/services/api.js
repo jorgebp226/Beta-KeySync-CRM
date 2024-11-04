79eb95d6bdc0144b19be3e319da470218fb5a163
jorgebp226 <jorgebp226@gmail.com>
2024-11-04 18:48:48 +0100

    Cambios en la UI de crear cuenta 9

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,6 @@
 import axios from 'axios';
-import { fetchAuthSession } from 'aws-amplify/auth';
 
 const api = axios.create();
-
-// Interceptor para añadir el token de autenticación
-api.interceptors.request.use(async (config) => {
-  try {
-    const session = await fetchAuthSession();
-    const token = session.tokens?.accessToken.toString();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-      config.headers['Content-Type'] = 'application/json'; // Agrega el Content-Type a cada solicitud
-    }
-    return config;
-  } catch (error) {
-    return Promise.reject(error);
-  }
-});
 
 // Verificar si el usuario tiene CRM
 export const checkLeadsStatus = async (sub) => {
@@ -31,12 +15,7 @@ export const getUserLeads = async (sub) => {
 
 // Inicializar WhatsApp y obtener QR
 export const initializeWhatsApp = async (sub) => {
-  const data = {
-    maxChats: 10,
-    maxMessagesPerChat: 1000,
-  };
-
-  return api.post(`http://13.36.209.139:3000/start/${sub}`, data);
+  return api.post(`https://wa9rhtsgbf.execute-api.eu-west-3.amazonaws.com/keysync/start${sub}`);
 };
 
 // Obtener código QR
